3238bd42f45c66bc86a6a203fcdad4c7cd15d301
strin <stl501@gmail.com>
2014-12-06 21:29:49 +0800

    remove profiling statements

--- DLGM/dlgm.py
+++ DLGM/dlgm.py
@@ -283,42 +283,27 @@ class DeepLatentGM(object):
       for si in range(me.num_sample):
         "first sample stochastic variables."
 
-        ta = time.clock()
         eps = rmodel.sample_eps(v)
         xi = rmodel.sample(v, eps)
-        tb = time.clock()
-        print 'time[sample]', tb-ta
 
         # pdb.set_trace()
-        ta = time.clock()
         "compute gradient of generative model."
         gg = gmodel.get_grad(v, *xi)
         gg = param_neg(gg)
         grad_g = param_add(grad_g, gg)
-        tb = time.clock()
-        print 'time[grad of generative]', tb-ta
 
         "compute gradient of regularizer in generative model."
-        ta = time.clock()
         gg_reg = gmodel.get_grad_reg()
         gg_reg = param_mul_scalar(gg_reg, me.kappa)
         grad_g = param_add(grad_g, gg_reg)
-        tb = time.clock()
-        print 'time[grad of regularizer]', tb-ta
 
         "compute free-energy gradient of recognition model."
-        ta = time.clock()
         gr = rmodel.get_grad(v)
         grad_r = param_add(grad_r, gr) 
-        tb = time.clock()
-        print 'time[grad of recog energy]', tb-ta
 
         "compute stochastic gradient of recognition model."
-        ta = time.clock()
         gg_xi = gmodel.get_grad_xi(v, *xi)
         gg_xi = param_neg(gg_xi)
-        tb = time.clock()
-        print 'time[grad of stoc grad]', tb-ta
 
         # compute hessian: strategy 1. (numerically unstable)
         # hh_xi = gmodel.get_hess_xi(v, *xi)
